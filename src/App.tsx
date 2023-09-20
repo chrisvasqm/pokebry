e37a7ddedab13e1b6da8c3579ef2b482ad1bd08b
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-20 00:36:26 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Container, Heading } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <Container size={['md', 'sm']}>
+      <Heading>Pokedex App</Heading>
+    </Container>
+  );
+}
+
+export default App;

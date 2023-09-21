506777a0f871396c99277a377fa98650d6e73129
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-21 18:27:57 -0400

    Remove extra Square

--- src/components/Pokedex.tsx
+++ src/components/Pokedex.tsx
@@ -18,20 +18,18 @@ function Pokedex() {
 
   return (
     <>
-      <Square placeContent={'start'} padding={8} borderRadius={5} backgroundColor={'#F00222'}>
-        <HStack>
-          <Circle backgroundColor={'#01FBFB'} size={'60px'} borderWidth={4} />
+      <Square placeContent={'start'} padding={4} borderRadius={5} backgroundColor={'#F00222'}>
+        <VStack align={'left'}>
+          <HStack>
+            <Circle backgroundColor={'#01FBFB'} size={'60px'} borderWidth={4} />
 
-          <Flex paddingBottom={10} gap={2}>
-            <Circle backgroundColor={'#EE031D'} size={3} borderWidth={2} borderColor={'black'} />
-            <Circle backgroundColor={'#CEB900'} size={3} borderWidth={2} borderColor={'black'} />
-            <Circle backgroundColor={'#217E44'} size={3} borderWidth={2} borderColor={'black'} />
-          </Flex>
-        </HStack>
-      </Square>
+            <Flex paddingBottom={10} gap={2}>
+              <Circle backgroundColor={'#EE031D'} size={3} borderWidth={2} borderColor={'black'} />
+              <Circle backgroundColor={'#CEB900'} size={3} borderWidth={2} borderColor={'black'} />
+              <Circle backgroundColor={'#217E44'} size={3} borderWidth={2} borderColor={'black'} />
+            </Flex>
+          </HStack>
 
-      <Square placeContent={'start'} padding={6} borderRadius={5} backgroundColor={'#F00222'}>
-        <VStack>
           <HStack>
             <Circle backgroundColor={'#519AFB'} size={'50px'} borderWidth={4} />
 

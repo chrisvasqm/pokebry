65b7b90feba51b7c466c60bc393bf63bbcf4eba8
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-21 19:51:14 -0400

    Improve screen layout

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -2,34 +2,44 @@ import { Box, Center, HStack, Heading, Square, VStack } from '@chakra-ui/react';
 
 function Screen() {
   return (
-    <HStack
+    <VStack
       className='section-middle'
       marginY={4}
       backgroundColor={'white'}
       borderRadius={6}
       borderWidth={10}
+      padding={6}
+      gap={10}
       borderColor={'gray.400'}
-      minHeight={'400px'}
+      minHeight={'360px'}
       minWidth={'100%'}
     >
-      <VStack>
-        <Center flex={2}>
-          <Heading>Name</Heading>
-        </Center>
+      <Heading>Name</Heading>
 
-        <HStack>
-          <Square minHeight={'50px'} minWidth={'50px'} backgroundColor={'gray'} />
-          <Box>
-            <p>HP: 100</p>
-            <p>Attack: 100</p>
-            <p>Defense: 100</p>
-            <p>Special-Attack: 100</p>
-            <p>Special-Defense: 100</p>
-            <p>Speed: 100</p>
-          </Box>
-        </HStack>
-      </VStack>
-    </HStack>
+      <HStack gap={10}>
+        <Square minHeight={'50px'} minWidth={'50px'} backgroundColor={'gray'} />
+        <Box>
+          <p>
+            <b>HP:</b> 100
+          </p>
+          <p>
+            <b>Attack:</b> 100
+          </p>
+          <p>
+            <b>Defense:</b> 100
+          </p>
+          <p>
+            <b>Special-Attack:</b> 100
+          </p>
+          <p>
+            <b>Special-Defense:</b> 100
+          </p>
+          <p>
+            <b>Speed:</b> 100
+          </p>
+        </Box>
+      </HStack>
+    </VStack>
   );
 }
 

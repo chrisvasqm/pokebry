13bafa1a0420f2ea753a50d4419b922b1da7cd0f
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-21 19:24:18 -0400

    Add sample data

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -1,18 +1,34 @@
-import { HStack, Square } from '@chakra-ui/react';
+import { Box, Center, HStack, Heading, Square, VStack } from '@chakra-ui/react';
 
 function Screen() {
   return (
-    <HStack className='section-middle' marginY={4}>
-      <Square
-        backgroundColor={'white'}
-        borderRadius={6}
-        borderWidth={10}
-        borderColor={'gray.400'}
-        minHeight={'400px'}
-        minWidth={'100%'}
-      >
-        Hola
-      </Square>
+    <HStack
+      className='section-middle'
+      marginY={4}
+      backgroundColor={'white'}
+      borderRadius={6}
+      borderWidth={10}
+      borderColor={'gray.400'}
+      minHeight={'400px'}
+      minWidth={'100%'}
+    >
+      <VStack>
+        <Center flex={2}>
+          <Heading>Name</Heading>
+        </Center>
+
+        <HStack>
+          <Square minHeight={'50px'} minWidth={'50px'} backgroundColor={'gray'} />
+          <Box>
+            <p>HP: 100</p>
+            <p>Attack: 100</p>
+            <p>Defense: 100</p>
+            <p>Special-Attack: 100</p>
+            <p>Special-Defense: 100</p>
+            <p>Speed: 100</p>
+          </Box>
+        </HStack>
+      </VStack>
     </HStack>
   );
 }

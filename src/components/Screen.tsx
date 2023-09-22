e1606a72937062995016bfbd117893d1eb42e55e
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-21 21:33:30 -0400

    Cleanup code

--- src/components/Screen.tsx
+++ src/components/Screen.tsx
@@ -6,6 +6,15 @@ interface Props {
 }
 
 function Screen({ pokemon }: Props) {
+  const stats = {
+    hp: pokemon?.stats[0].base_stat,
+    attack: pokemon?.stats[1].base_stat,
+    defense: pokemon?.stats[2].base_stat,
+    specialAttack: pokemon?.stats[3].base_stat,
+    specialDefense: pokemon?.stats[4].base_stat,
+    speed: pokemon?.stats[5].base_stat
+  };
+
   return (
     <VStack
       className='section-middle'
@@ -28,22 +37,22 @@ function Screen({ pokemon }: Props) {
         <Image src={pokemon?.sprites.front_default} width={'150px'} height={'150px'} />
         <Box>
           <p>
-            <b>HP:</b> {pokemon?.stats[0].base_stat}
+            <b>HP:</b> {stats.hp}
           </p>
           <p>
-            <b>Attack:</b> {pokemon?.stats[1].base_stat}
+            <b>Attack:</b> {stats.attack}
           </p>
           <p>
-            <b>Defense:</b> {pokemon?.stats[2].base_stat}
+            <b>Defense:</b> {stats.defense}
           </p>
           <p>
-            <b>Special-Attack:</b> {pokemon?.stats[3].base_stat}
+            <b>Special-Attack:</b> {stats.specialAttack}
           </p>
           <p>
-            <b>Special-Defense:</b> {pokemon?.stats[4].base_stat}
+            <b>Special-Defense:</b> {stats.specialDefense}
           </p>
           <p>
-            <b>Speed:</b> {pokemon?.stats[5].base_stat}
+            <b>Speed:</b> {stats.speed}
           </p>
         </Box>
       </HStack>

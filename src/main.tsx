74389e5338f4588324b53fd8d47124792250f04e
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-16 23:22:26 -0400

    Setup React Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>
 );

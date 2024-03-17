962a551843b626d4e8620e320b6ab811d544a334
chrisvasqm <christianvasquezmelo@gmail.com>
2024-03-16 23:39:15 -0400

    Specify types

--- src/hooks/usePokemonById.ts
+++ src/hooks/usePokemonById.ts
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import service from '../services/pokemonService';
+import Pokemon from "../models/Pokemon";
 
 const usePokemonById = (id: number) => {
-    return useQuery({
+    return useQuery<Pokemon, Error>({
         queryKey: ['pokemon', id],
         queryFn: () => service.find(id),
         placeholderData: prev => prev

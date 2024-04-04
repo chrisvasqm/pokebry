949aa65332e870b2e5efa3023bbcb266c7b33bd4
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-04 00:09:38 -0400

    Remove unused code

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -15,11 +15,6 @@ class APIClient<T> {
         return instance.get<T>(`${this.endpoint}/${id}`)
             .then(response => response.data);
     }
-
-    search = (name: string) => {
-        return instance.get<T>(`${this.endpoint}/${name}`)
-            .then(response => response.data);
-    }
 }
 
 export default APIClient;

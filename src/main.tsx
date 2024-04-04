77231474a61937cf783b666baddc24ffbdf90e28
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-04 00:15:51 -0400

    Remove unused files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';

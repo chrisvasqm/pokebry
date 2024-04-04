0706323164e8252260d3f31d49d47b05085ac9bc
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-04 15:54:43 -0400

    Fix heading pointer event being too large

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 import { Card, Heading } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import useTitleStore from "../store/useTitleStore";
 
 const NavBar = () => {
@@ -6,7 +7,9 @@ const NavBar = () => {
 
     return (
         <Card paddingY={2} paddingX={4} borderRadius={0} boxShadow={'md'}>
-            <Heading fontSize={'2xl'}>{title}</Heading>
+            <Heading as={'h1'} fontSize={'2xl'}>
+                <Link to='/pokemons'>{title}</Link>
+            </Heading>
         </Card>
     )
 }

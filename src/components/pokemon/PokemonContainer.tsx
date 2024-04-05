14deab4aff3fdea02842f68e5732ed78640b0402
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-05 11:40:17 -0400

    Add infinite scrolling

--- src/components/pokemon/PokemonContainer.tsx
+++ src/components/pokemon/PokemonContainer.tsx
@@ -1,17 +1,31 @@
-import { Button, Center, SimpleGrid, Spinner } from "@chakra-ui/react";
+import { Center, SimpleGrid, Spinner } from "@chakra-ui/react";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import usePokemons from "../../hooks/usePokemons";
 import PokemonCard from "./PokemonCard";
 
 const PokemonsContainer = () => {
-    const { data, isLoading, error, isFetchingNextPage, fetchNextPage } = usePokemons({ limit: 52 });
+    const {
+        data,
+        isLoading,
+        error,
+        fetchNextPage,
+        hasNextPage
+    } = usePokemons({ limit: 52 });
 
     if (isLoading) return <Center height={'100vh'}><Spinner /></Center>
 
     if (error) return <p>{error.message}</p>
 
+    const fetchedPokemonsCount = data?.pages.reduce((total, page) => total + page.length, 0) || 0;
+
     return (
-        <>
+        <InfiniteScroll
+            dataLength={fetchedPokemonsCount}
+            hasMore={hasNextPage}
+            next={fetchNextPage}
+            loader={<Spinner />}
+        >
             <SimpleGrid gap={3} columns={[2, 3, 4, 5, 6]}>
                 {data?.pages.map((page, index) =>
                     <React.Fragment key={index}>
@@ -19,15 +33,7 @@ const PokemonsContainer = () => {
                     </React.Fragment>
                 )}
             </SimpleGrid>
-            <Center marginY={2}>
-                <Button
-                    colorScheme="blue"
-                    isLoading={isLoading || isFetchingNextPage}
-                    onClick={() => fetchNextPage()}>
-                    Load More
-                </Button>
-            </Center>
-        </>
+        </InfiniteScroll>
     )
 }
 

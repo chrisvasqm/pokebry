dede68141e2c5c8b16681c9120fcfe9d293b2b3c
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-05 12:16:46 -0400

    Add color mode toggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,20 @@
-import { Card, Heading } from "@chakra-ui/react";
+import { MoonIcon } from "@chakra-ui/icons";
+import { Card, HStack, Heading, Spacer, Switch, useColorMode } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
     return (
         <Card borderRadius={0} boxShadow={'md'}>
-            <Heading marginY={2} marginLeft={4} as={'h1'} fontSize={'3xl'}>
-                <Link to='/pokemons'>PokéDex</Link>
-            </Heading>
+            <HStack>
+                <Heading marginY={2} marginLeft={4} as={'h1'} fontSize={'3xl'}>
+                    <Link to='/pokemons'>PokéDex</Link>
+                </Heading>
+                <Spacer />
+                <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+                <MoonIcon marginLeft={1} marginRight={4} />
+            </HStack>
         </Card>
     )
 }

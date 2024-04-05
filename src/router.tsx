7394fa143f29b213dbf8847657a9fdca2337652d
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-05 12:30:09 -0400

    Add error page

--- src/router.tsx
+++ src/router.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from 'react-router-dom'
 import Layout from './Layout'
 import PokemonDetail from './components/pokemon/PokemonDetail'
 import PokemonsContainer from './components/pokemon/PokemonContainer'
+import ErrorPage from './components/ErrorPage'
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
+        errorElement: <ErrorPage />,
         children: [
             { path: '/pokemons', element: <PokemonsContainer /> },
             { path: '/pokemons/:id', element: <PokemonDetail /> }

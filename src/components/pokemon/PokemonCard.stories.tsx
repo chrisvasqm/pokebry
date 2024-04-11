924a7c32b8ff5deca0d15f42d45d1a850ebb7efa
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-11 00:01:17 -0400

    Add pokemon detail story

--- src/components/pokemon/PokemonCard.stories.tsx
+++ src/components/pokemon/PokemonCard.stories.tsx
@@ -1,9 +1,25 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import PokemonCard from './PokemonCard';
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const meta: Meta<typeof PokemonCard> = {
     component: PokemonCard,
+    decorators: [
+        (Story) => (
+            <ChakraProvider>
+                <QueryClientProvider client={queryClient}>
+                    <BrowserRouter>
+                        <Story />
+                    </BrowserRouter>
+                </QueryClientProvider>
+            </ChakraProvider>
+        )
+    ]
 }
 
 export default meta;
---
+++ src/components/pokemon/PokemonDetail.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import PokemonDetail from './PokemonDetail';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ChakraProvider } from '@chakra-ui/react';
+
+const queryClient = new QueryClient();
+
+const meta: Meta<typeof PokemonDetail> = {
+    component: PokemonDetail,
+    decorators: [
+        (Story) => (
+            <ChakraProvider>
+                <QueryClientProvider client={queryClient}>
+                    <MemoryRouter initialEntries={['/pokemons/1']}>
+                        <Routes>
+                            <Route element={<Story />} path='/pokemons/:id' />
+                        </Routes>
+                    </MemoryRouter>
+                </QueryClientProvider>
+            </ChakraProvider>
+        )
+    ]
+}
+
+export default meta;
+
+type Story = StoryObj<typeof PokemonDetail>;
+
+export const Default: Story = {}

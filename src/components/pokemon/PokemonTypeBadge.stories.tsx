42cba7d6a826bb5a43c69f87e1cefbc238f26f35
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-11 00:13:47 -0400

    Add type badge story

---
+++ src/components/pokemon/PokemonTypeBadge.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { MemoryRouter } from 'react-router-dom';
+import PokemonTypeBadge from './PokemonTypeBadge';
+
+const meta: Meta<typeof PokemonTypeBadge> = {
+    component: PokemonTypeBadge,
+    decorators: [
+        (Story) => (
+            <ChakraProvider>
+                <MemoryRouter>
+                    <Story />
+                </MemoryRouter>
+            </ChakraProvider>
+        )
+    ]
+}
+
+export default meta;
+
+type Story = StoryObj<typeof PokemonTypeBadge>;
+
+export const Default: Story = {
+    args: {
+        pokemon: {
+            types: [
+                { type: { name: "fire" } }
+            ]
+        }
+    }
+}

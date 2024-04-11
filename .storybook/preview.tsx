d03f729c2ab061301a8fd481297dd22585ff9a9a
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-11 00:00:31 -0400

    Remove global decorator

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,24 +1,5 @@
 import type { Preview } from "@storybook/react";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-
-const queryClient = new QueryClient();
-
-export const decorators = [
-  (Story) => (
-    <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Story />
-        </BrowserRouter>
-      </QueryClientProvider>
-    </ChakraProvider>
-  ),
-];
-
 const preview: Preview = {
   parameters: {
     controls: {

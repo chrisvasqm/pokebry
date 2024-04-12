14d719a6f467cedf7db6a6b2b41d0943b127e721
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-12 17:46:44 -0400

    Change NavBar layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { MoonIcon } from "@chakra-ui/icons";
-import { Card, HStack, Heading, Spacer, Switch, useColorMode } from "@chakra-ui/react";
+import { Card, Container, HStack, Heading, Spacer, Switch, useColorMode } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const NavBar = () => {
@@ -7,14 +7,16 @@ const NavBar = () => {
 
     return (
         <Card borderRadius={0} boxShadow={'md'}>
-            <HStack>
-                <Heading marginY={2} marginLeft={4} as={'h1'} fontSize={'3xl'}>
-                    <Link to='/pokemons'>Pokebry</Link>
-                </Heading>
-                <Spacer />
-                <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-                <MoonIcon marginLeft={1} marginRight={4} />
-            </HStack>
+            <Container maxWidth={'4xl'} paddingX={2}>
+                <HStack>
+                    <Heading marginY={2} as={'h1'} fontSize={'3xl'}>
+                        <Link to='/pokemons'>Pokebry</Link>
+                    </Heading>
+                    <Spacer />
+                    <Switch isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+                    <MoonIcon marginLeft={1} />
+                </HStack>
+            </Container>
         </Card>
     )
 }

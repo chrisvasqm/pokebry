a085874ff1c557eb341c9d718393b01d3dd3dd2e
chrisvasqm <christianvasquezmelo@gmail.com>
2024-04-12 21:05:37 -0400

    Fix NavBar layout

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,14 +7,16 @@ const NavBar = () => {
 
     return (
         <Card id='navbar' borderRadius={0} boxShadow={'md'}>
-            <HStack>
-                <Heading marginY={2} marginLeft={4} as={'h1'} fontSize={'3xl'}>
-                    <Link id='navbar-title' to='/pokemons'>Pokebry</Link>
-                </Heading>
-                <Spacer />
-                <Switch id='navbar-switch' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
-                <MoonIcon id='navbar-moon-icon' marginLeft={1} marginRight={4} />
-            </HStack>
+            <Container maxWidth={'4xl'}>
+                <HStack>
+                    <Heading marginY={2} as={'h1'} fontSize={'3xl'}>
+                        <Link id='navbar-title' to='/pokemons'>Pokebry</Link>
+                    </Heading>
+                    <Spacer />
+                    <Switch id='navbar-switch' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+                    <MoonIcon id='navbar-moon-icon' marginLeft={1} />
+                </HStack>
+            </Container>
         </Card>
     )
 }
